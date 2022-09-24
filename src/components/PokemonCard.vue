0d06a8f898ae60d9439a64636d0f1c16140171f0
RFTaurus <rakish_fanquer@pahamify.com>
2022-09-24 17:36:23 +0700

    Finishing Card Component
    Finishing Card Component

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" />
+    </div>
+    <div class="card-content">
+      <p class="text-pokemon-number">No.001</p>
+      <p class="text-pokemon-name"><strong>Bulbasaur</strong></p>
+      <br />
+      <p class="text-pokemon-title">max CP:</p>
+      <p class="text-pokemon-cp">951</p>
+      <p class="text-pokemon-title">Type</p>
+      <div class="row chip justify-content-center pt-2">
+        <div class="col">
+          <span>Grass</span>
+        </div>
+        <div class="col">
+          <span>Grass</span>
+        </div>
+        <div class="col">
+          <span>Grass</span>
+        </div>
+        <div class="col">
+          <span>Grass</span>
+        </div>
+        <div class="col">
+          <span>Grass</span>
+        </div>
+        <div class="col">
+          <span>Grass</span>
+        </div>
+        <div class="col">
+          <span>Grass</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped>
+.card-header .logo {
+  transform: scale(1);
+  width: 100%;
+  max-width: 128px;
+  height: auto;
+}
+
+.card-content {
+  padding: var(--padding-gap);
+  color: #f6f6f6;
+  font-size: 1.15em;
+}
+
+.card-content .text-pokemon-number {
+  font-size: 1.1em;
+}
+.card-content .text-pokemon-name {
+  font-size: 1.25em;
+}
+.card-content .text-pokemon-number {
+  font-size: 1.1em;
+}
+.card-content .text-pokemon-cp {
+  color: #f68522;
+  font-size: 1.2em;
+  font-weight: bold;
+}
+
+.card-content .chip {
+  width: 100%;
+  max-width: 240px;
+}
+
+.card-content .chip span {
+  background: #78c850;
+  width: fit-content;
+  padding: 2px var(--padding-gap);
+  margin: 4px auto;
+  font-size: 0.8em;
+  font-weight: bold;
+  border-radius: var(--border-radius-full);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,72 @@
+<template>
+  <div class="container">
+    <div class="page-header text-center">
+      <h1>PokemonDex Arena List Card</h1>
+    </div>
+    <div></div>
+    <div class="row align-items-center justify-content-center text-center">
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+      <div class="col col-md-6 col-lg-3 my-4">
+        <PokemonCard />
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import PokemonCard from "../components/PokemonCard.vue";
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="css" scoped>
+.page-header {
+  color: #f68522;
+  font-size: 1em;
+}
+
+@media (min-width: 768px) {
+  .page-header {
+    font-size: 1.5em;
+  }
+}
+</style>
