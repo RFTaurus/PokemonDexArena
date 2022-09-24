297d244ea7b7fc74db307373efe444b7bbb6b0e1
RFTaurus <rakish_fanquer@pahamify.com>
2022-09-24 14:44:20 +0700

    Clean Unused Component
    Clean Unused Component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
@@ -19,6 +18,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
   <RouterView />
 </template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
 
 <style scoped>
 header {
